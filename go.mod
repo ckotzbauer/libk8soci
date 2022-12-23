bd3f36fa6c82f4cda97e8ad642b05aefa5d3cefe
Christian Kotzbauer <git@ckotzbauer.de>
2022-12-23 16:42:57 +0100

    feat: removed SaveImage function and added ConvertSecrets
    Signed-off-by: Christian Kotzbauer <git@ckotzbauer.de>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ckotzbauer/libk8soci
 go 1.19
 
 require (
+	github.com/anchore/stereoscope v0.0.0-20221208011002-c5ff155d72f1
 	github.com/docker/cli v20.10.22+incompatible
 	github.com/go-git/go-git/v5 v5.5.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
@@ -15,15 +16,18 @@ require (
 require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20221026131551-cf6655e29de4 // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/anchore/go-logger v0.0.0-20220728155337-03b66a5207d8 // indirect
+	github.com/bmatcuk/doublestar/v4 v4.0.2 // indirect
 	github.com/cloudflare/circl v1.1.0 // indirect
-	github.com/containerd/stargz-snapshotter/estargz v0.12.1 // indirect
+	github.com/containerd/containerd v1.6.12 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
@@ -34,7 +38,10 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -49,24 +56,34 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc2 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pjbgf/sha1cd v0.2.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
-	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/scylladb/go-set v1.0.3-0.20200225121959-cc7b2070d91e // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/skeema/knownhosts v1.1.0 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/vbatts/tar-split v0.11.2 // indirect
+	github.com/sylabs/sif/v2 v2.8.1 // indirect
+	github.com/sylabs/squashfs v0.6.1 // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	github.com/wagoodman/go-partybus v0.0.0-20200526224238-eb215533f07d // indirect
+	github.com/wagoodman/go-progress v0.0.0-20200621122631-1a2120f0695a // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/mod v0.6.0 // indirect
 	golang.org/x/net v0.3.1-0.20221206200815-1e63c2f08a10 // indirect
 	golang.org/x/oauth2 v0.1.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/term v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	golang.org/x/tools v0.1.12 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
+	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
--- pkg/oci/registry.go
+++ pkg/oci/registry.go
@@ -2,106 +2,17 @@ package oci
 
 import (
 	"bytes"
-	"fmt"
 
+	"github.com/anchore/stereoscope/pkg/image"
 	"github.com/docker/cli/cli/config"
 	"github.com/docker/cli/cli/config/configfile"
 	"github.com/docker/cli/cli/config/types"
 	"github.com/google/go-containerregistry/pkg/authn"
-	"github.com/google/go-containerregistry/pkg/crane"
 	"github.com/google/go-containerregistry/pkg/name"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
-	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/sirupsen/logrus"
 
 	parser "github.com/novln/docker-parser"
 )
-
-var (
-	ErrorNoValidPullSecret = fmt.Errorf("No valid or valid-but-unauthorized PullSecret found from ContainerImage")
-)
-
-func SaveImage(imagePath string, image RegistryImage) error {
-	o := crane.GetOptions()
-	var err error
-	var cfg types.AuthConfig
-	empty := types.AuthConfig{}
-
-	if len(image.PullSecrets) == 0 {
-		_, err := downloadImage(imagePath, image, o)
-		if err != nil {
-			logrus.WithError(err).Error()
-		}
-
-		return err
-	} else {
-		for _, pullSecret := range image.PullSecrets {
-			cfg, err = ResolveAuthConfigWithPullSecret(image, *pullSecret)
-			if err != nil {
-				logrus.WithError(err).Warnf("image: %s, Read authentication configuration from secret: %s failed", image.ImageID, pullSecret.SecretName)
-				continue
-			}
-
-			if cfg != empty {
-				o.Remote = []remote.Option{
-					remote.WithAuth(authn.FromConfig(authn.AuthConfig{
-						Username:      cfg.Username,
-						Password:      cfg.Password,
-						Auth:          cfg.Auth,
-						IdentityToken: cfg.IdentityToken,
-						RegistryToken: cfg.RegistryToken,
-					})),
-				}
-			}
-
-			proceed, err := downloadImage(imagePath, image, o)
-			if !proceed && err == nil {
-				logrus.Debugf("Image %s successfully pulled with PullSecret: %s", image.ImageID, pullSecret.SecretName)
-				return nil
-			} else if !proceed && err != nil {
-				logrus.WithError(err).Error()
-				return err
-			} else if proceed && err != nil {
-				logrus.WithError(err).Debug()
-			}
-		}
-	}
-
-	// no valid pull request found for this image - returning an error
-	return ErrorNoValidPullSecret
-}
-
-func downloadImage(imagePath string, image RegistryImage, o crane.Options) (bool, error) {
-	imageMap := map[string]v1.Image{}
-	ref, err := name.ParseReference(image.ImageID, o.Name...)
-
-	if err != nil {
-		// should stop immediately because it seems that no other pullSecret will solve this problem
-		return false, fmt.Errorf("parsing reference %q: %w", image.ImageID, err)
-	}
-
-	rmt, err := remote.Get(ref, o.Remote...)
-	if err != nil {
-		// should continue, because, this might be an Authentication Error
-		return true, fmt.Errorf("image: %s, Image-Pull Error: %w", image.ImageID, err)
-	}
-
-	img, err := rmt.Image()
-	if err != nil {
-		// should stop immediately because no other pullSecret will solve this problem
-		return false, err
-	}
-
-	imageMap[image.ImageID] = img
-
-	if err := crane.MultiSave(imageMap, imagePath); err != nil {
-		// should stop immediately because no other pullSecret will solve this problem
-		return false, fmt.Errorf("saving tarball %s: %w", imagePath, err)
-	}
-
-	// pull was sucessfull - no error occurred
-	return false, nil
-}
 
 func ResolveAuthConfigWithPullSecret(image RegistryImage, pullSecret KubeCreds) (types.AuthConfig, error) {
 	var cf *configfile.ConfigFile
@@ -149,4 +60,24 @@ func ResolveAuthConfig(image RegistryImage) (types.AuthConfig, error) {
 	} else {
 		return types.AuthConfig{}, nil
 	}
+}
+
+func ConvertSecrets(img RegistryImage) []image.RegistryCredentials {
+	credentials := make([]image.RegistryCredentials, 0)
+	for _, secret := range img.PullSecrets {
+		cfg, err := ResolveAuthConfigWithPullSecret(img, *secret)
+		if err != nil {
+			logrus.WithError(err).Warnf("image: %s, Read authentication configuration from secret: %s failed", img.ImageID, secret.SecretName)
+			continue
+		}
+
+		credentials = append(credentials, image.RegistryCredentials{
+			Username:  cfg.Username,
+			Password:  cfg.Password,
+			Token:     cfg.RegistryToken,
+			Authority: cfg.ServerAddress,
+		})
+	}
+
+	return credentials
 }
